4ca3963c7ef1b2a63430bce2cd2fd147f67e0036
Léo Crotet <leo.crotet@nfrance.com>
2018-07-16 17:53:26 +0200

    add js

---
+++ exec.php
@@ -0,0 +1,25 @@
+<?php 
+if (isset($_POST['RedON']))
+{
+exec('sudo python /var/www/gpio/red_on.py');
+}
+if (isset($_POST['RedOFF']))
+{
+exec('sudo python /var/www/gpio/red_off.py');
+}
+if (isset($_POST['YellowON']))
+{
+exec('sudo python /var/www/gpio/yellow_on.py');
+}
+if (isset($_POST['YellowOFF']))
+{
+exec('sudo python /var/www/gpio/yellow_off.py');
+}
+if (isset($_POST['GreenON']))
+{
+exec('sudo python /var/www/gpio/green_on.py');
+}
+if (isset($_POST['GreenOFF']))
+{
+exec('sudo python /var/www/gpio/green_off.py');
+}
---
+++ proceed.js
@@ -0,0 +1,8 @@
+function proceed () {
+    var form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', 'http://127.0.0.1/exec.php');
+    form.style.display = 'hidden';
+    document.body.appendChild(form)
+    form.submit();
+}
